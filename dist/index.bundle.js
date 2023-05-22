3942aadb442960b7b42e9f0c42c58a9c60f49e66
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-05-22 15:33:50 -0600

    Setup webpack with Typescript

---
+++ dist/index.bundle.js
@@ -0,0 +1,25 @@
+(self["webpackChunkodin_cv"] = self["webpackChunkodin_cv"] || []).push([["index"],{
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/***/ (() => {
+
+function component() {
+    var element = document.createElement("div");
+    (element.innerHTML = "Hello"), "webpack";
+    return element;
+}
+document.body.appendChild(component());
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.ts"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vb2Rpbi1jdi8uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiBjb21wb25lbnQoKSB7XG4gICAgdmFyIGVsZW1lbnQgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KFwiZGl2XCIpO1xuICAgIChlbGVtZW50LmlubmVySFRNTCA9IFwiSGVsbG9cIiksIFwid2VicGFja1wiO1xuICAgIHJldHVybiBlbGVtZW50O1xufVxuZG9jdW1lbnQuYm9keS5hcHBlbmRDaGlsZChjb21wb25lbnQoKSk7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
