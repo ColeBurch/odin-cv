f904c5ba44183116acc22d42467c17add6f4e46c
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-05-25 16:22:49 -0600

    Add github link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
+const githubSVG = require("../images/github.svg");
 
 function Footer() {
   return (
-    <footer className="flex justify-center">
-      <p className="text-gray-600 text-sm my-4">Made by Cole Burch</p>
+    <footer className="flex justify-center items-center">
+      <p className="text-black text-xl my-4">Made by Cole Burch</p>
+      <a href="https://github.com/ColeBurch">
+        <img src={githubSVG} alt="GitHub" className="w-5 h-5 mx-2" />
+      </a>
     </footer>
   );
 }
--- src/components/MainBox.tsx
+++ src/components/MainBox.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 function MainBox() {
   return (
-    <div className="flex flex-col flex-grow bg-gray-100 rounded-lg shadow-lg overflow-hidden"></div>
+    <div className="flex flex-col flex-grow bg-gray-100 rounded-lg shadow-lg overflow-hidden min-h-screen"></div>
   );
 }
 
