f8a8904ae7fc1eacc336955ded1658895784e1b6
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-06-01 10:59:27 -0600

    Add all info to PersonalInfoForm and style.

--- src/components/MainBox.tsx
+++ src/components/MainBox.tsx
@@ -3,7 +3,7 @@ import PersonalInfoForm from "./PersonalInfoForm";
 
 function MainBox() {
   return (
-    <div className="flex flex-col flex-grow bg-gray-100 rounded-lg shadow-lg overflow-hidden min-h-screen">
+    <div className="flex flex-col flex-grow bg-gray-100 rounded-lg shadow-lg overflow-hidden min-h-screen items-center">
       <PersonalInfoForm />
     </div>
   );
--- src/components/PersonalInfoForm.tsx
+++ src/components/PersonalInfoForm.tsx
@@ -4,7 +4,11 @@ import { Component } from "react";
 type PersonalInfoFormProps = {};
 
 type PersonalInfoFormState = {
-  name: string;
+  firstName: string;
+  lastName: string;
+  address: string;
+  phoneNumber: number;
+  email: string;
 };
 
 class PersonalInfoForm extends Component<
@@ -15,29 +19,94 @@ class PersonalInfoForm extends Component<
     super(props);
 
     this.state = {
-      name: "",
+      firstName: "",
+      lastName: "",
+      address: "",
+      phoneNumber: null,
+      email: "",
     };
   }
 
-  handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  handleFirstNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     this.setState({
-      name: event.target.value,
+      firstName: event.target.value,
+    });
+  };
+
+  handleLastNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      lastName: event.target.value,
+    });
+  };
+
+  handleAddressChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      address: event.target.value,
+    });
+  };
+
+  handlePhoneNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      phoneNumber: Number(event.target.value),
+    });
+  };
+
+  handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      email: event.target.value,
     });
   };
 
   handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    alert(`${this.state.name}`);
+    alert(
+      `${this.state.firstName} , ${this.state.lastName} , ${this.state.address} , ${this.state.phoneNumber} , ${this.state.email}`
+    );
     event.preventDefault();
   };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>Name</label>
+      <form
+        onSubmit={this.handleSubmit}
+        className="flex flex-col flex-grow box-border w-1/2 p-4 gap-3 bg-gray-300"
+      >
+        <h2 className="text-2xl font-bold text-gray-800">
+          Personal Information
+        </h2>
         <input
           type="text"
-          value={this.state.name}
-          onChange={this.handleNameChange}
+          placeholder="First Name"
+          value={this.state.firstName}
+          onChange={this.handleFirstNameChange}
+          className="rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={this.state.lastName}
+          onChange={this.handleLastNameChange}
+          className="rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="Address"
+          value={this.state.address}
+          onChange={this.handleAddressChange}
+          className="rounded-lg"
+        />
+        <input
+          type="tel"
+          placeholder="Phone Number"
+          value={this.state.phoneNumber}
+          onChange={this.handlePhoneNumberChange}
+          className="rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={this.state.email}
+          onChange={this.handleEmailChange}
+          className="rounded-lg"
         />
         <button type="submit">Submit</button>
       </form>
